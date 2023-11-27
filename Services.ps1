75cd0b090bd3c0fffc0d8ae375127f88d63e87b4
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2023-11-27 10:58:52 +0000

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -6,8 +6,8 @@ List of services and state can be found here: https://raw.githubusercontent.com/
 #>
 # Create CSV based on Windows Server 2019
 $CSVURL = "https://raw.githubusercontent.com/ITR-MITHO/ServerScripts/main/Services.csv"
-$fileContent = Invoke-RestMethod -Uri $CSVURL
-$fileContent | Out-File -FilePath "$home\desktop\Services.csv"
+$CSVFile = Invoke-RestMethod -Uri $CSVURL
+$CSVFile | Out-File -FilePath "$home\desktop\Services.csv"
 
 timeout 5 | Out-Null
 $Data = Import-csv $home\desktop\Services.csv
