175e178368b7d0d918e14ecf182ba09f241c1c7a
Mikkel Thodsen <123701392+ITR-MITHO@users.noreply.github.com>
2024-01-29 11:39:00 +0000

    Update CPU-RAM-USAGE.ps1

--- CPU-RAM-USAGE.ps1
+++ CPU-RAM-USAGE.ps1
@@ -1,28 +1,45 @@
-<#
-Run locally on the server you want to perform the check upon. It will find the Top 3 users and processes that uses your CPU and RAM.
-Requires elevated permissions, and permissions to write on the fileshare
-#>
-
-Echo "Timestamp, Server, CPUinUse, TopCPUProccess, TopCPUUser, RAMinUse, TopRAMProccess, TopRAMUser" | Out-File \\Server\Share\Performance.csv -Encoding unicode
-$Processor = (Get-WmiObject -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average
-$ComputerMemory = Get-WmiObject -Class win32_operatingsystem -ErrorAction Stop
-$Memory = ((($ComputerMemory.TotalVisibleMemorySize - $ComputerMemory.FreePhysicalMemory)*100)/ $ComputerMemory.TotalVisibleMemorySize)
-$RoundMemory = [math]::Round($Memory, 2)
-
+# Static variables
 $Computername = Hostname
 $Date = (Get-Date -Format "dd/MM/yyyy HH:mm:ss")
-        
-$CPUProcesses = Get-Process | Sort-Object CPU -Descending | Select-Object -First 3
-$RAMProcesses = Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 3
 
+# CPU Measurement / Threshhold = 85%
+$Processor = (Get-WmiObject -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average
+If ($Processor -GT 85)
+{
+$CPUFile = Test-Path "\\domain.com\SHARE\CPU.csv"
+If (-Not $CPUFile)
+{
+Echo "Timestamp, Server, CPULoad, Process, User" | Out-File \\domain.com\SHARE\CPU.csv -Encoding unicode
+}
+
+$CPUProcesses = Get-Process | Sort-Object CPU -Descending | Select-Object -First 3
 $CPUProcesses | ForEach-Object {
 $CPUOwner = (Get-WmiObject -Query "SELECT * FROM Win32_Process WHERE ProcessId=$($_.Id)").GetOwner()
 $TopCPUProcess =  $($_.ProcessName) 
 $TopCPUUser = $($CPUowner.User)
+Echo "$Date, $Computername, $Processor%, $TopCPUprocess, $TopCPUUser" | Out-File \\domain.com\SHARE\CPU.csv -Encoding unicode -Append
 
+}
+    }
 
-$RAMowner = (Get-WmiObject -Query "SELECT * FROM Win32_Process WHERE ProcessId=$($_.Id)").GetOwner()
+    # RAM Measurement / Threshold = 85%
+$RAMProcesses = Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 3
+$ComputerMemory = Get-WmiObject -Class win32_operatingsystem -ErrorAction Stop
+$Memory = ((($ComputerMemory.TotalVisibleMemorySize - $ComputerMemory.FreePhysicalMemory)*100)/ $ComputerMemory.TotalVisibleMemorySize)
+$RoundMemory = [math]::Round($Memory, 2)
+
+If ($RoundMemory -GT 85)
+{
+$RAMFile = Test-Path "\\domain.com\SHARE\RAM.csv"
+If (-Not $RAMFile)
+{
+Echo "Timestamp, Server RAMLoad, Process, User" | Out-File \\domain.com\SHARE\RAM.csv -Encoding unicode
+}
+
+$RAMProcesses | ForEach-Object {
+$RAMOwner = (Get-WmiObject -Query "SELECT * FROM Win32_Process WHERE ProcessId=$($_.Id)").GetOwner()
 $TopRAMProcess =  $($_.ProcessName)
 $TopRAMUser = $($RAMowner.User)
-
-Echo "$Date, $Computername, $Processor, $TopCPUprocess, $TopCPUUser, $RoundMemory, $TopRAMProcess, $TopRamUser" | Out-File \\Server\Share\Performance.csv -Encoding unicode -Append}
+Echo "$Date, $Computername, $RoundMemory%, $TopRAMProcess, $TopRamUser" | Out-File \\domain.com\SHARE\RAM.csv -Encoding unicode -Append
+}
+    }
